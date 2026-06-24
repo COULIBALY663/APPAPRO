c32d0f93f69f10a658120b93140c29efbdad724f
COULIBALY663 <ziec2061@gmail.com>
2026-06-24 18:06:21 +0000

    dlnd

--- dahboard/src/pages/Dashboard.jsx
+++ dahboard/src/pages/Dashboard.jsx
@@ -142,7 +142,7 @@ export default function Dashboard() {
 
           {/* LE LIEN EST MAINTENANT ICI, DANS LE FORMULAIRE */}
           {!isRegisterMode && (
-            <p onClick={() => window.location.href = "/reset-password"} 
+            <p onClick={() => window.location.href = "/ForgotPassword"} 
                style={{ cursor: "pointer", color: "#6c757d", textAlign: "center", marginTop: "15px", fontSize: "14px", textDecoration: "underline" }}>
                Mot de passe oublié ?
             </p>
