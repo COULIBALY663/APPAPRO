e533cedd381cb333c9d513dd4dfb8e66a42586a0
COULIBALY663 <ziec2061@gmail.com>
2026-07-03 20:21:34 +0000

    csbjkj

--- dahboard/public/sw.js
+++ dahboard/public/sw.js
@@ -1,10 +1,27 @@
 // public/sw.js
-self.addEventListener('push', (event) => {
+
+self.addEventListener("push", (event) => {
+  if (!event.data) return;
+
   const data = event.data.json();
+
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: '//favicon.svg' 
+    self.registration.showNotification(data.title || "Nouvelle notification", {
+      body: data.body || "",
+      icon: "/favicon.svg",
+      badge: "/favicon.svg",
+      vibrate: [200, 100, 200],
+      tag: "dashboard-admin",
+      renotify: true,
+      requireInteraction: true,
     })
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("/")
   );
 });
