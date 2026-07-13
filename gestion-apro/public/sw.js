abae3ec275ea76586fe4670bf4248315752a328d
COULIBALY663 <ziec2061@gmail.com>
2026-07-13 19:02:25 +0000

    d ;f,g

---
+++ gestion-apro/public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Ne fait rien, laisse passer les requêtes directement vers Render
+  return;
+});
